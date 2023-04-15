0bebde0d98f666e5c909486a72b571aa3cdea5f7
Colin Mackay <github@colinmackay.scot>
2023-04-15 17:09:57 +0100

    (#49) Create example program to show off analyser

---
+++ Example/Example/Program.cs
@@ -0,0 +1,51 @@
+﻿using Stravaig.Extensions.Core;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetInputCheckingWithHasContent();
+            GetInputCheckingWithStringIsNullOrWhiteSpace();
+        }
+
+        private static void GetInputCheckingWithHasContent()
+        {
+            Console.WriteLine("HasContent");
+            Console.WriteLine("Type something and press Enter!");
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+            if (input.HasContent())
+            {
+                Console.WriteLine($"You inputted: {input}");
+            }
+            else
+            {
+                Console.WriteLine("You did not input anything.");
+            }
+        }
+
+
+        private static void GetInputCheckingWithStringIsNullOrWhiteSpace()
+        {
+            // This method should be picked up by the Stravaig.Extensions.Core.Analyzer
+            // It should detect that
+            // !string.IsNullOrWhiteSpace(input)
+            // can be replaced by
+            // input.HasContent()
+            Console.WriteLine("!string.IsNUllOrWhiteSpace");
+            Console.WriteLine("Type something and press Enter!");
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine($"You inputted: {input}");
+            }
+            else
+            {
+                Console.WriteLine("You did not input anything.");
+            }
+        }
+    }
+}
