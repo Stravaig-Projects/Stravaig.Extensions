8b36da7abf4ed9f2ef7c5642ca21306b4c0ece5d
Colin Mackay <github@colinmackay.scot>
2023-04-15 17:23:51 +0100

    (#49) Extract alternatives to separate files

---
+++ Example/Example/NotStringIsNullOrWhiteSpace.cs
@@ -0,0 +1,28 @@
+﻿namespace Example
+{
+    internal static class NotStringIsNullOrWhiteSpace
+    {
+
+        internal static void GetInput()
+        {
+            // This method should be picked up by the Stravaig.Extensions.Core.Analyzer
+            // It should detect that
+            // !string.IsNullOrWhiteSpace(input)
+            // can be replaced by
+            // input.HasContent()
+            // It should also suggest the inclusion of the Stravaig.Extensions.Core namespace
+            Console.WriteLine("!string.IsNUllOrWhiteSpace");
+            Console.WriteLine("Type something and press Enter!");
+            Console.Write("> ");
+            string? input = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine($"You inputted: {input}");
+            }
+            else
+            {
+                Console.WriteLine("You did not input anything.");
+            }
+        }
+    }
+}
--- Example/Example/Program.cs
+++ Example/Example/Program.cs
@@ -1,51 +1,11 @@
-﻿using Stravaig.Extensions.Core;
-
-namespace Example
+﻿namespace Example
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            GetInputCheckingWithHasContent();
-            GetInputCheckingWithStringIsNullOrWhiteSpace();
-        }
-
-        private static void GetInputCheckingWithHasContent()
-        {
-            Console.WriteLine("HasContent");
-            Console.WriteLine("Type something and press Enter!");
-            Console.Write("> ");
-            string? input = Console.ReadLine();
-            if (input.HasContent())
-            {
-                Console.WriteLine($"You inputted: {input}");
-            }
-            else
-            {
-                Console.WriteLine("You did not input anything.");
-            }
-        }
-
-
-        private static void GetInputCheckingWithStringIsNullOrWhiteSpace()
-        {
-            // This method should be picked up by the Stravaig.Extensions.Core.Analyzer
-            // It should detect that
-            // !string.IsNullOrWhiteSpace(input)
-            // can be replaced by
-            // input.HasContent()
-            Console.WriteLine("!string.IsNUllOrWhiteSpace");
-            Console.WriteLine("Type something and press Enter!");
-            Console.Write("> ");
-            string? input = Console.ReadLine();
-            if (!string.IsNullOrWhiteSpace(input))
-            {
-                Console.WriteLine($"You inputted: {input}");
-            }
-            else
-            {
-                Console.WriteLine("You did not input anything.");
-            }
+            HasContent.GetInput();
+            NotStringIsNullOrWhiteSpace.GetInput();
         }
     }
 }
