3fc01f9566bf33772eb0b818df7657091f7efc71
Colin Mackay <github@colinmackay.scot>
2023-04-17 08:30:48 +0100

    (#49) Tidy

--- src/Stravaig.Extensions.Core.Analyzer/Stravaig.Extensions.Core.Analyzer/Sec0001UseStringHasContentAnalyzer.cs
+++ src/Stravaig.Extensions.Core.Analyzer/Stravaig.Extensions.Core.Analyzer/Sec0001UseStringHasContentAnalyzer.cs
@@ -3,10 +3,8 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System;
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
-using System.Threading;
 
 namespace Stravaig.Extensions.Core.Analyzer
 {
