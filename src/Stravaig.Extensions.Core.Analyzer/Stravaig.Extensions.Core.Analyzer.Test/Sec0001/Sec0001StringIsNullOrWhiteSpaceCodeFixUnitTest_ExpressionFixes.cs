215befb9205f3682ff5bcd9ca2eec4a5e6e7ecbd
Colin Mackay <github@colinmackay.scot>
2023-04-21 14:14:37 +0100

    (#49) Refactor code fix unit tests and fill in missing ones

--- src/Stravaig.Extensions.Core.Analyzer/Stravaig.Extensions.Core.Analyzer.Test/Sec0001/Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest_ExpressionFixes.cs
+++ src/Stravaig.Extensions.Core.Analyzer/Stravaig.Extensions.Core.Analyzer.Test/Sec0001/Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest_ExpressionFixes.cs
@@ -7,12 +7,13 @@ using static Stravaig.Extensions.Core.Analyzer.Test.CSharpCodeFixVerifier<
 namespace Stravaig.Extensions.Core.Analyzer.Test.Sec0001;
 
 [TestFixture]
-public class Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest
+public partial class Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest
 {
     [Test]
     public async Task NotStringIsNullOrWhiteSpaceStringArg()
     {
-        const string test = @"using System;
+        const string test = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -23,7 +24,7 @@ class MyClass
 }";
 
         const string fix = @"using Stravaig.Extensions.Core;
-using System;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -39,7 +40,8 @@ class MyClass
     [Test]
     public async Task NotStringIsNullOrWhiteSpaceStringArgAsExpression()
     {
-        const string test = @"using System;
+        const string test = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -50,7 +52,7 @@ class MyClass
 }";
 
         const string fix = @"using Stravaig.Extensions.Core;
-using System;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -67,6 +69,7 @@ class MyClass
     public async Task StringIsNullOrWhiteSpaceStringArgEqualsFalse()
     {
         const string test = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -77,12 +80,41 @@ class MyClass
 }";
 
         const string fix = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
     public bool MyMethod(string someString)
     {
         return (someString.HasContent());
+    }
+}";
+
+        await VerifyCodeFixAsync(test, fix);
+    }
+
+    [Test]
+    public async Task StringIsNullOrWhiteSpaceStringExpressionArgEqualsFalse()
+    {
+        const string test = @"using Stravaig.Extensions.Core;
+
+namespace MyNamespace;
+class MyClass
+{
+    public bool MyMethod(int aNumber)
+    {
+        return ([|string.IsNullOrWhiteSpace(aNumber.ToString()) == false|]);
+    }
+}";
+
+        const string fix = @"using Stravaig.Extensions.Core;
+
+namespace MyNamespace;
+class MyClass
+{
+    public bool MyMethod(int aNumber)
+    {
+        return (aNumber.ToString().HasContent());
     }
 }";
 
@@ -93,6 +125,7 @@ class MyClass
     public async Task FalseEqualsStringIsNullOrWhiteSpaceStringArg()
     {
         const string test = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -103,6 +136,7 @@ class MyClass
 }";
 
         const string fix = @"using Stravaig.Extensions.Core;
+
 namespace MyNamespace;
 class MyClass
 {
@@ -116,14 +150,16 @@ class MyClass
     }
 
     [Test]
-    public async Task NoUsingDeclarationsNotStringIsNullOrWhiteSpaceStringArg()
+    public async Task FalseEqualsStringIsNullOrWhiteSpaceStringExpressionArg()
     {
-        const string test = @"namespace MyNamespace;
+        const string test = @"using Stravaig.Extensions.Core;
+
+namespace MyNamespace;
 class MyClass
 {
-    public bool MyMethod(string someString)
+    public bool MyMethod(int aNumber)
     {
-        return ([|!string.IsNullOrWhiteSpace(someString)|]);
+        return ([|false == string.IsNullOrWhiteSpace(aNumber.ToString())|]);
     }
 }";
 
@@ -132,94 +168,9 @@ class MyClass
 namespace MyNamespace;
 class MyClass
 {
-    public bool MyMethod(string someString)
+    public bool MyMethod(int aNumber)
     {
-        return (someString.HasContent());
-    }
-}";
-        await VerifyCodeFixAsync(test, fix);
-    }
-
-    [Test]
-    public async Task UsingDeclarationExistsButNotInAlphabeticalOrderNotStringIsNullOrWhiteSpaceStringArg()
-    {
-        const string test = @"using System;
-using Stravaig.Extensions.Core;
-
-namespace MyNamespace;
-class MyClass
-{
-    public bool MyMethod(string someString)
-    {
-        return ([|!string.IsNullOrWhiteSpace(someString)|]);
-    }
-}";
-
-        const string fix = @"using System;
-using Stravaig.Extensions.Core;
-
-namespace MyNamespace;
-class MyClass
-{
-    public bool MyMethod(string someString)
-    {
-        return (someString.HasContent());
-    }
-}";
-        await VerifyCodeFixAsync(test, fix);
-    }
-
-    [Test]
-    public async Task UsingDeclarationsInsideNamespaceNotStringIsNullOrWhiteSpaceStringArg()
-    {
-        const string test = @"namespace MyNamespace
-{
-    using System;
-
-    class MyClass
-    {
-        public bool MyMethod(string someString)
-        {
-            return ([|!string.IsNullOrWhiteSpace(someString)|]);
-        }
-    }
-}";
-
-        const string fix = @"namespace MyNamespace
-{
-    using Stravaig.Extensions.Core;
-    using System;
-
-    class MyClass
-    {
-        public bool MyMethod(string someString)
-        {
-            return (someString.HasContent());
-        }
-    }
-}";
-
-        await VerifyCodeFixAsync(test, fix);
-    }
-
-    [Test]
-    public async Task NoNamespaceUsingDeclarationPutAtTopOfFile()
-    {
-        const string test = @"class MyClass
-{
-    public bool MyMethod(string someString)
-    {
-        return ([|!string.IsNullOrWhiteSpace(someString)|]);
-    }
-}";
-
-        const string fix = @"using Stravaig.Extensions.Core;
-
-class MyClass
-{
-    public bool MyMethod(string someString)
-    {
-        return (someString.HasContent());
+        return (aNumber.ToString().HasContent());
     }
 }";
 
---
+++ src/Stravaig.Extensions.Core.Analyzer/Stravaig.Extensions.Core.Analyzer.Test/Sec0001/Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest_NoNamespaceFixes.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using System.Threading.Tasks;
+using static Stravaig.Extensions.Core.Analyzer.Test.CSharpCodeFixVerifier<
+    Stravaig.Extensions.Core.Analyzer.Sec0001UseStringHasContentAnalyzer,
+    Stravaig.Extensions.Core.Analyzer.Sec0001UseStringHasContentAnalyzerCodeFixProvider>;
+
+namespace Stravaig.Extensions.Core.Analyzer.Test.Sec0001;
+
+[TestFixture]
+public partial class Sec0001StringIsNullOrWhiteSpaceCodeFixUnitTest
+{
+    [Test]
+    public async Task NoUsingDeclarationsWithoutNamespace()
+    {
+        const string test = @"class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return ([|!string.IsNullOrWhiteSpace(someString)|]);
+    }
+}";
+
+        const string fix = @"using Stravaig.Extensions.Core;
+
+class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return (someString.HasContent());
+    }
+}";
+        await VerifyCodeFixAsync(test, fix);
+    }
+
+    [Test]
+    public async Task UsingDeclarationExistsWithoutNamespace()
+    {
+        const string test = @"using System;
+using Stravaig.Extensions.Core;
+
+class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return ([|!string.IsNullOrWhiteSpace(someString)|]);
+    }
+}";
+
+        const string fix = @"using System;
+using Stravaig.Extensions.Core;
+
+class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return (someString.HasContent());
+    }
+}";
+        await VerifyCodeFixAsync(test, fix);
+    }
+
+    [Test]
+    public async Task UsingDeclarationMissingWithOthersWithoutNamespace()
+    {
+        const string test = @"using System;
+
+class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return ([|!string.IsNullOrWhiteSpace(someString)|]);
+    }
+}";
+
+        const string fix = @"using Stravaig.Extensions.Core;
+using System;
+
+class MyClass
+{
+    public bool MyMethod(string someString)
+    {
+        return (someString.HasContent());
+    }
+}";
+        await VerifyCodeFixAsync(test, fix);
+    }
+
+}
