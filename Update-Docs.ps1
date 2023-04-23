493f5373fb12d8b5286bd14f47ef3c11cb8c12d5
Colin Mackay <github@colinmackay.scot>
2023-04-23 22:32:12 +0100

    Update Release notes docs

--- Update-Docs.ps1
+++ Update-Docs.ps1
@@ -9,14 +9,16 @@ $releaseNotesIndexFile = @(
 ""
 );
 
-Get-ChildItem "$PSScriptRoot/docs/release-notes/release-notes*.md" |
+Get-ChildItem "$PSScriptRoot/release-notes/release-notes*.md" |
     Sort-Object -Descending -Property Name |
     ForEach-Object -Process {
         $name = $_.Name;
         $releaseDate = $_.LastWriteTimeUtc.ToString("dddd, d MMMM yyyy 'at' HH:mm");
         $null = $name -match "release-notes-(?<version>[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\.md";
         $version = $Matches.version;
-        $releaseNotesIndexFile += "* [v$version]($name) released on $releaseDate UTC";
+        $releaseNotesIndexFile += "- **[v$version]($name) released on $releaseDate UTC**";
+        $releaseNotesIndexFile += "  - [GitHub Release](https://github.com/Stravaig-Projects/Stravaig.Extensions.Core/releases/tag/v$version)"
+        $releaseNotesIndexFile += "  - [Nuget Package](https://www.nuget.org/packages/Stravaig.Extensions.Core/$version)"
     }
 
 Set-Content "$PSScriptRoot/docs/release-notes/index.md" $releaseNotesIndexFile -Encoding UTF8 -Force
